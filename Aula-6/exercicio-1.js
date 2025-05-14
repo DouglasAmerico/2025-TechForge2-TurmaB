76002b97e2b4d575a650bedabc100b066bb42399
Douglas Américo <douglas@likesistemas.com.br>
2025-05-13 22:44:43 -0300

    aula sobre objectos

---
+++ Aula-6/exercicio-1.js
@@ -0,0 +1,20 @@
+let jogo = {
+    nome: "GTA",
+    anoDeLancamento: 2007,
+    preco: 50.50,
+    desenvolvedor: "RockStar",
+    estilo: "RPG",
+    plataforma: "PC"
+}
+
+console.log(jogo);
+
+let filme = {
+    nome: "Titanic",
+    diretor: "James cameron",
+    anoLancamento: 1998,
+    genero: "drama"
+}
+
+console.log(filme);
+
